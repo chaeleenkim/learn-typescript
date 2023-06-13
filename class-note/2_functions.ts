1e843499efaf71beade29c7e65f0a6bb971cc761
chaeleen <cl27au@gmail.com>
2023-06-13 19:44:05 +0900

    [Sec 03]타입스크립트 기초 - 함수 타입

---
+++ class-note/2_functions.ts
@@ -0,0 +1,18 @@
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+  return 10;
+}
+
+// 함수의 타입을 정의하는 방식
+function sumNum(a: number, b: number): number {
+  return a + b;
+}
+
+// 함수의 스펙과 일치하지 않으면 오류 발생
+// sumNum(10,20,30,40)
+
+// 함수의 옵셔널 파라미터
+// 물음표로 필수가 아닌 파라미터를 정의 가능
+function log(a: string, b?: string) {}
+
+log("hello ts");
